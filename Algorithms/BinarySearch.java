256f889984c8e58bfdd4beacd3a17d849c9ef585
mercurial_harsh <harshguptaucme@gmail.com>
2020-01-10 17:32:54 +0530

    Day2 of 100daysof code

---
+++ Algorithms/BinarySearch.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class BinarySearch {
+
+    static int search(int[] xs, int x) {
+        int pivot = xs.length / 2;
+
+        if (x == xs[pivot]) {
+            return 1;
+        }
+        if (xs.length == 1) {
+            return -1;
+        }
+        if (x < xs[pivot]) {
+            return search(Arrays.copyOfRange(xs, 0, pivot - 1), x);
+        }
+        if (x > xs[pivot]) {
+            return search(Arrays.copyOfRange(xs, pivot, xs.length), x);
+        }
+        return -1;
+    }
+}
---
+++ Algorithms/QuickSort.java
@@ -0,0 +1,35 @@
+class QuickSort {
+
+    static void quicksort(int[] xs) {
+        quicksort(xs, 0, xs.length - 1);
+    }
+
+    static void quicksort(int[] xs, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivot = xs[(left + right) / 2];
+        int index = partition(xs, left, right, pivot);
+        quicksort(xs, left, index - 1);
+        quicksort(xs, index, right);
+    }
+
+    static int partition(int[] xs, int left, int right, int pivot) {
+        while (left <= right) {
+            while (xs[left] < pivot) {
+                left++;
+            }
+            while (xs[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int tmp = xs[left];
+                xs[left] = xs[right];
+                xs[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+}
