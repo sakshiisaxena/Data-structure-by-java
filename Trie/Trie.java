21ab42b5849b209a785c672fc558133ed96ff165
mercurial_harsh <harshguptaucme@gmail.com>
2020-01-11 23:58:52 +0530

    update

---
+++ Trie/Trie.java
@@ -0,0 +1,26 @@
+class Trie {
+
+    Node root = new Node();
+
+    void insert(String s) {
+        Node current = root;
+        for (char c : s.toCharArray()) {
+            current.children.putIfAbsent(c, new Node());
+            current = current.children.get(c);
+        }
+        current.isWord = true;
+    }
+
+    boolean isWord(String s) {
+        Node current = root;
+
+        for (char c : s.toCharArray()) {
+            current = current.children.get(c);
+
+            if (current == null) {
+                return false;
+            }
+        }
+        return current.isWord;
+    }
+}
